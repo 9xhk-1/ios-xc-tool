156f124b1dc6b8410ed843979be0a446c8175f9e
ios-xc-tool <admin@ios-xc.tool>
2026-07-20 00:14:29 +0800

    Fix Cheat files

--- src/Cheat.h
+++ src/Cheat.h
@@ -1,7 +1,5 @@
-﻿#pragma once
-#include <mach/mach.h>
+#pragma once
 extern bool gAutoShoot;
 extern float gAimSpeed;
 extern float gFovRadius;
-namespace MemX { extern mach_port_t gTask; bool Init(pid_t pid); }
 void CheatTick();
